d30f853e964bbf81c23b806e194d3859ce28296d
Mehmet SUNAY <85196101+mehmetsunay@users.noreply.github.com>
2022-07-07 14:54:35 +0300

    tc-kimlik-sorgula
    tc-kimlik-sorgula

---
+++ WEBSERVICE-KIMLIKDOGRULAMA-main/WinFormWebServices/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WinFormWebServices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_tc = new System.Windows.Forms.TextBox();
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.txt_soyisim = new System.Windows.Forms.TextBox();
+            this.txt_yil = new System.Windows.Forms.TextBox();
+            this.btn_gonder = new System.Windows.Forms.Button();
+            this.lbl_tc = new System.Windows.Forms.Label();
+            this.lbl_isim = new System.Windows.Forms.Label();
+            this.lbl_soyisim = new System.Windows.Forms.Label();
+            this.lbl_yil = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_tc
+            // 
+            this.txt_tc.Location = new System.Drawing.Point(160, 43);
+            this.txt_tc.Name = "txt_tc";
+            this.txt_tc.Size = new System.Drawing.Size(247, 22);
+            this.txt_tc.TabIndex = 0;
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Location = new System.Drawing.Point(160, 90);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(247, 22);
+            this.txt_isim.TabIndex = 1;
+            // 
+            // txt_soyisim
+            // 
+            this.txt_soyisim.Location = new System.Drawing.Point(160, 146);
+            this.txt_soyisim.Name = "txt_soyisim";
+            this.txt_soyisim.Size = new System.Drawing.Size(247, 22);
+            this.txt_soyisim.TabIndex = 2;
+            // 
+            // txt_yil
+            // 
+            this.txt_yil.Location = new System.Drawing.Point(160, 216);
+            this.txt_yil.Name = "txt_yil";
+            this.txt_yil.Size = new System.Drawing.Size(247, 22);
+            this.txt_yil.TabIndex = 3;
+            // 
+            // btn_gonder
+            // 
+            this.btn_gonder.Location = new System.Drawing.Point(160, 282);
+            this.btn_gonder.Name = "btn_gonder";
+            this.btn_gonder.Size = new System.Drawing.Size(247, 36);
+            this.btn_gonder.TabIndex = 4;
+            this.btn_gonder.Text = "GÖNDER";
+            this.btn_gonder.UseVisualStyleBackColor = true;
+            this.btn_gonder.Click += new System.EventHandler(this.btn_gonder_Click);
+            // 
+            // lbl_tc
+            // 
+            this.lbl_tc.AutoSize = true;
+            this.lbl_tc.Location = new System.Drawing.Point(38, 47);
+            this.lbl_tc.Name = "lbl_tc";
+            this.lbl_tc.Size = new System.Drawing.Size(30, 17);
+            this.lbl_tc.TabIndex = 5;
+            this.lbl_tc.Text = "TC:";
+            this.lbl_tc.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_isim
+            // 
+            this.lbl_isim.AutoSize = true;
+            this.lbl_isim.Location = new System.Drawing.Point(38, 95);
+            this.lbl_isim.Name = "lbl_isim";
+            this.lbl_isim.Size = new System.Drawing.Size(38, 17);
+            this.lbl_isim.TabIndex = 6;
+            this.lbl_isim.Text = "İSİM:";
+            // 
+            // lbl_soyisim
+            // 
+            this.lbl_soyisim.AutoSize = true;
+            this.lbl_soyisim.Location = new System.Drawing.Point(38, 151);
+            this.lbl_soyisim.Name = "lbl_soyisim";
+            this.lbl_soyisim.Size = new System.Drawing.Size(67, 17);
+            this.lbl_soyisim.TabIndex = 7;
+            this.lbl_soyisim.Text = "SOYİSİM:";
+            // 
+            // lbl_yil
+            // 
+            this.lbl_yil.AutoSize = true;
+            this.lbl_yil.Location = new System.Drawing.Point(38, 221);
+            this.lbl_yil.Name = "lbl_yil";
+            this.lbl_yil.Size = new System.Drawing.Size(32, 17);
+            this.lbl_yil.TabIndex = 8;
+            this.lbl_yil.Text = "YIL:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(540, 376);
+            this.Controls.Add(this.lbl_yil);
+            this.Controls.Add(this.lbl_soyisim);
+            this.Controls.Add(this.lbl_isim);
+            this.Controls.Add(this.lbl_tc);
+            this.Controls.Add(this.btn_gonder);
+            this.Controls.Add(this.txt_yil);
+            this.Controls.Add(this.txt_soyisim);
+            this.Controls.Add(this.txt_isim);
+            this.Controls.Add(this.txt_tc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "TC KİMLİK DOĞRULAMA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_tc;
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.TextBox txt_soyisim;
+        private System.Windows.Forms.TextBox txt_yil;
+        private System.Windows.Forms.Button btn_gonder;
+        private System.Windows.Forms.Label lbl_tc;
+        private System.Windows.Forms.Label lbl_isim;
+        private System.Windows.Forms.Label lbl_soyisim;
+        private System.Windows.Forms.Label lbl_yil;
+    }
+}
+
---
+++ WEBSERVICE-KIMLIKDOGRULAMA-main/WinFormWebServices/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormWebServices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_gonder_Click(object sender, EventArgs e)
+        {
+            /*
+             WEB SERVİSLER HTTP PROTOKOLUNU KULLANARAK HİZMET SAĞLARLAR 
+             İSTEMCİ YANİ CLİENT İSTEK ATAR VE BİR GERİ DÖNÜŞ GERÇEKLEŞİR 
+             BU GERİ DÖNÜŞTE KULLLANILAN SERVİSE GÖRE XML , JSON VB YAPILAR KULLANILIR
+             VE BİLGİ ALIŞVERİŞİ SAĞLANMIŞ OLUR
+             BURADA TEXTBOXLAR ÜZERİNDEN GİRİLEN VERİLER ALINIP SORGULAMA YAPILIR VE GERİ DÖNÜŞ TİP OLARAK 
+             TRUE YADA FALSE OLAN BOOL BİR DEĞER DÖNDÜRDÜK.
+             AYRICA PROJEMİZE ADD SERVİCE REFERENCE DİYEREK DE URL MİZİ GEÇİRDİK
+             
+              
+             */
+
+
+            long kimlikNo = long.Parse(txt_tc.Text);
+            int yil = int.Parse(txt_yil.Text);
+            bool durum;
+
+            try
+            {
+                using (TcDogrula.KPSPublicSoapClient service =new TcDogrula.KPSPublicSoapClient { })
+                {
+                    durum = service.TCKimlikNoDogrula(kimlikNo, txt_isim.Text, txt_soyisim.Text, yil);
+
+                }
+
+            }
+
+            catch(Exception)
+            {
+                throw;
+
+            }
+
+
+            MessageBox.Show(durum.ToString());
+
+        }
+    }
+}
